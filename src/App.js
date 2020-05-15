31d154b9f4db1df116f1e5a62463647b176d8b05
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-05-14 20:08:06 -0400

    Started NASA APOD Module 2 advanced styling.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios'
 import { BASE_URL, API_KEY } from './constants/index.js'
+
+////////////////////////////////
+///// IMPORTING COMPONENTS /////
+////////////////////////////////
 import Header from './components/Header/Header'
 import Description from './components/Description/Description'
 import Nasaimage from './components/NASAImage/Nasaimage'
 
+///////////////////////////////////////
+///// IMPORTING STYLED COMPONENTS /////
+///////////////////////////////////////
+import styled from 'styled-components'
+import { StyledHeaderContainer } from './components/Header/Header'
+
+
+
+/////////////////////////////
+///// PARENT COMPONENT //////
+/////////////////////////////
 function App() {
   const [data, setData] = useState({})
   const { date, title, explanation, url } = data
@@ -22,13 +37,12 @@ function App() {
 
   return (
     <div className="App">
+      {/* <StyledHeaderContainer className="placeholder">
+            <StyledLogo className="placeholder">&lt; 💅 &gt;</StyledLogo>
+      </StyledHeaderContainer> */}
       <Header date={date} title={title} />
       <Description explanation={explanation} />
       <Nasaimage url={url} />
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from "react";
+import styled from 'styled-components'
+
+const StyledHeaderContainer = styled.div`
+    background-image: url(https://images.unsplash.com/photo-1451188502541-13943edb6acb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80);
+    font-size: 1.6rem;
+    padding: 5% 8%;
+`
+
+const StyledLogo = styled.div`
+    display: inline-block;
+    padding: 2rem 1rem;
+    font-size: 1.4rem;
+`;
 
 const Header = (props) => {
     return (
+        // <StyledHeaderContainer className="placeholder">
+        //     <StyledLogo className="placeholder">&lt; 💅 &gt;</StyledLogo>
+        // </StyledHeaderContainer>
         <div className="placeholder">
             <p>{props.date}</p>
             <h2>{props.title}</h2>
